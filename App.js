84999cb9e79dd5774f26432dc39fa7bd23526f75
mka_kir <kir1711mol@gmail.com>
2021-10-06 15:17:11 +0300

    finish hw-2

--- App.js
+++ App.js
@@ -1,28 +1,17 @@
-import React, {useState} from 'react';
-import { StyleSheet, View } from 'react-native';
+import React from 'react';
+import {StyleSheet} from 'react-native';
 
-import { StatusBar } from 'expo-status-bar';
-
-import Start from "./src/components/start/start";
-import Login from "./src/components/Login/Login";
-import SignUp from "./src/components/SignUp/SignUp";
-
+import Tabs from "./src/navigation/Tabs";
 export default function App() {
-    const [screen, setScreen] = useState('start')
-    const goBack = ()=> setScreen('start')
+
   return (
-    <View style={styles.container}>
-        {screen === 'start' && <Start setScreen={setScreen}/> }
-        {screen === 'login' && <Login goBack={goBack}/>}
-        {screen === 'signUp' && <SignUp goBack={goBack}/>}
-      <StatusBar style="auto" />
-    </View>
+        <Tabs/>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#fff',
-      height: '100%',
-  },
+
 });
+
+
+
---
+++ src/screens/BookShop.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+
+export default function BookShop() {
+
+    return (
+        <View style={styles.container}>
+            <Text>BookShop Screen</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+});
