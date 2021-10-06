7220be9ad5df62c55d598d9ce35feea9b62d2178
mka_kir <kir1711mol@gmail.com>
2021-10-06 19:40:29 +0300

    change tab navigation styles

--- src/navigation/Tabs.js
+++ src/navigation/Tabs.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import {StyleSheet} from 'react-native';
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import Home from "../screens/Home"
 import AllBooks from "../screens/AllBooks";
@@ -18,46 +18,47 @@ export default function Tabs() {
     return (
         <NavigationContainer>
             <Tab.Navigator
-                screenOptions={({route})=>({
-                    tabBarIcon:({focused,size,color})=>{
+                screenOptions={({route}) => ({
+                    headerStyle: {backgroundColor: '#000'},
+                    headerTintColor: 'red',
+                    headerTitleAlign: 'center',
+                    tabBarIcon: ({focused, size, color}) => {
                         let iconName;
-                        if (route.name === 'Home'){
-                            iconName='home' ;
-                            size=focused? 25:20;
-                            color=focused? 'red':'#000' ;
-                        }else if (route.name === 'All books'){
-                            iconName='book' ;
-                            size=focused? 25:20;
-                            color=focused? 'red':'#000'
-                        }else if (route.name === 'List Of Books'){
-                            iconName='bookmark';
-                            size=focused? 25:20;
-                            color=focused? 'red':'#000'
-                        }else if (route.name === 'Book Shop'){
-                            iconName='shopping-basket';
-                            size=focused? 25:20;
-                            color=focused? 'red':'#000'
+                        if (route.name === 'Home') {
+                            iconName = 'home';
+                            size = focused ? 25 : 20;
+                            color = focused ? 'red' : '#fff';
+                        } else if (route.name === 'All books') {
+                            iconName = 'book';
+                            size = focused ? 25 : 20;
+                            color = focused ? 'red' : '#fff'
+                        } else if (route.name === 'List Of Books') {
+                            iconName = 'bookmark';
+                            size = focused ? 25 : 20;
+                            color = focused ? 'red' : '#fff'
+                        } else if (route.name === 'Book Shop') {
+                            iconName = 'shopping-basket';
+                            size = focused ? 25 : 20;
+                            color = focused ? 'red' : '#fff'
                         }
-                        return(
-                            <FontAwesome5
-                                name={iconName}
-                                size={size}
-                                color={color}
-                            />
+                        return (
+                            <FontAwesome5 name={iconName} size={size} color={color}/>
                         )
-                    }
+                    },
+                    tabBarStyle: {
+                        backgroundColor: '#000',
+                        borderTopWidth: 0
+                    },
                 })
                 }
             >
-                <Tab.Screen name="Home" component={Home} options={{tabBarLabel: ()=> null}}/>
-                <Tab.Screen name="All books" component={AllBooks} options={{tabBarBadge: 3, tabBarLabel: ()=> null}}/>
-                <Tab.Screen name="List Of Books" component={ListOfBooksCurrently} options={{tabBarLabel: ()=> null}}/>
-                <Tab.Screen name="Book Shop" component={BookShop} options={{tabBarLabel: ()=> null}}/>
+                <Tab.Screen name="Home" component={Home} options={{tabBarLabel: () => null}} />
+                <Tab.Screen name="All books" component={AllBooks} options={{tabBarBadge: 3, tabBarLabel: () => null}}/>
+                <Tab.Screen name="List Of Books" component={ListOfBooksCurrently} options={{tabBarLabel: () => null}}/>
+                <Tab.Screen name="Book Shop" component={BookShop} options={{tabBarLabel: () => null}}/>
             </Tab.Navigator>
         </NavigationContainer>
     );
 }
 
-const styles = StyleSheet.create({
-
-});
+const styles = StyleSheet.create({});
